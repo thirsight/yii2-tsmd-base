72d65da6d3c0f81b9e77e95222994f7f9e051d7f
Haisenπ <thirsight@gmail.com>
2022-11-10 13:07:46 +0800

    add regUnicodeCJK pattern

--- config/params.php
+++ config/params.php
@@ -13,6 +13,13 @@ return [
     'supportEmail' => 'support@thirsight.com',
     'userRememberMe' => 86400 * 30,
 
+    // https://unicode-table.com/en/blocks/
+    // CJK Unified Ideographs Extension A
+    // CJK Unified Ideographs
+    // CJK Compatibility Ideographs
+    // CJK Compatibility Ideographs Supplement 張芳慈
+    'regUnicodeCJK' => '\x{3400}-\x{4DBF}\x{4E00}-\x{9FFF}\x{F900}-\x{FAFF}\x{2F800}-\x{30000}',
+
     'tsmd\base\controllers\RestController' => [
         'corsOrigin' => [
             'http://localhost:80',
